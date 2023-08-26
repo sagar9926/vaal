e801370fe109b54f1439ee125db22e7d23c8c758
sagar9926 <sagarkumar9926@gmail.com>
2023-08-26 23:05:57 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -31,7 +31,7 @@ class Solver:
 
 
     def train(self, querry_dataloader, val_dataloader, task_model, vae, discriminator, unlabeled_dataloader):
-        self.args = (self.args.num_images * self.args.train_epochs) // self.args.batch_size)
+        self.args = ((self.args.num_images * self.args.train_epochs) // self.args.batch_size)
         lr_change = self.args.train_iterations // 4
         labeled_data = self.read_data(querry_dataloader)
         unlabeled_data = self.read_data(unlabeled_dataloader, labels=False)
